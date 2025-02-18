67df0260517e29493ab41f81187b04b7795ac6fd
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-02-18 14:56:24 +0530

    session 4

---
+++ dotnetcore_sessions/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using dotnetcore_sessions.Data;
+using dotnetcore_sessions.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore_sessions.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
