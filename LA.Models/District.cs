707e70ffd52137e1c5cf46d4308ba02143aff698
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-02-26 13:15:56 +0530

    dotnet core session 6

---
+++ LA.Models/District.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA.Models
+{
+    public class District
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int StateId { get; set; }
+
+        //navigartion property
+        public State? State { get; set; }
+
+       
+    }
+}
---
+++ LA.Models/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int CountryId { get; set; }
+
+
+        //navigartion property
+        public Country? Country { get; set; }
+
+        public ICollection<District> districts { get; set; } = new HashSet<District>();
+    }
+}
